a857507c9cbcdd36a2470d47b11a0ea584d0bf62
jaydenpiao <jaydenpiao@gmail.com>
2024-01-11 18:53:37 -0800

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,7 +112,7 @@ const Navbar = () => {
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                     <a className='flex justify-between items-center w-full text-gray-300'
-                    href='https://drive.google.com/file/d/1y4bXWwGK4G06ufyMzc3qq5Y328F1sejU/view' target='_blank' rel='noopener noreferrer'>
+                    href='https://drive.google.com/file/d/1DZ5ENnf9C_oZidiCvQzff-c8bc4Znt-Z/view?usp=sharing' target='_blank' rel='noopener noreferrer'>
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
